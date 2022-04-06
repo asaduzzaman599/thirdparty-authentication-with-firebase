ae9385c628a7edb4b09b416386163ba0a0f27091
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-06 15:59:06 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './Components/SignIn/SignIn';
+import Profile from './Components/Profile/Profile';
+import { createContext, useState } from 'react';
 
-function App() {
+  const UserContext = createContext();
+
+  function App() {
+    const [user,setUser] = useState({});
   return (
-    <div className="App">
-     
-    </div>
+    <UserContext.Provider className="App" value={[user,setUser]}>
+      <Routes>
+        <Route path='/' element={<SignIn></SignIn>}></Route>
+        <Route path='/profile' element={<Profile></Profile>}></Route>
+      </Routes>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Profile.css'
+
+const Profile = () => {
+    return (
+        <div>
+            <h3>profile</h3>
+        </div>
+    );
+};
+
+export default Profile;
